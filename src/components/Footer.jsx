c300422720f227a265aa98da89e7b0fe3f9f9fab
tiho810 <zhayloobekov.06@gmail.com>
2023-06-14 14:39:54 +0600

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,45 +4,82 @@ const Footer = () => {
   return (
     <footer
       style={{
-        backgroundColor: "white",
+        backgroundColor: "#FF0000",
         bottom: 0,
         left: 0,
         right: 0,
+        position: "absolute",
+        flexWrap: "wrap",
+        boxShadow: "0px -4px 10px rgba(0, 0, 0, 0.2)",
       }}
     >
-      <section style={{ display: "flex", justifyContent: "space-between" }}>
-        <div style={{ display: "flex" }}>
-          <div style={{ marginRight: "20px" }}>
-            <h5 style={{ margin: 0, color: "black" }}>Company</h5>
-            <a href="#" style={{ color: "black", textDecoration: "none" }}>
-              About
-            </a>
-            <a href="#" style={{ color: "black", textDecoration: "none" }}>
-              Careers
-            </a>
+      <section style={{ display: "flex", justifyContent: "center" }}>
+        <div
+          style={{ display: "flex", fontWeight: "500", fontFamily: "arial" }}
+        >
+          <div
+            style={{
+              marginRight: "20px",
+              fontFamily: "arial",
+            }}
+          >
+            <h4 style={{ margin: 0, color: "black", fontWeight: "700" }}>
+              О компании
+            </h4>
+            <ul style={{ listStyleType: "none", padding: 0 }}>
+              <li>
+                <a href="#" style={{ color: "white", textDecoration: "none" }}>
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#" style={{ color: "white", textDecoration: "none" }}>
+                  Careers
+                </a>
+              </li>
+            </ul>
           </div>
           <div style={{ marginRight: "20px" }}>
-            <h5 style={{ margin: 0, color: "black" }}>Discover</h5>
-            <a href="#" style={{ color: "black", textDecoration: "none" }}>
-              Trust & Safety
-            </a>
-            <a href="#" style={{ color: "black", textDecoration: "none" }}>
-              Travel Credit
-            </a>
+            <h4 style={{ margin: 0, color: "black" }}>OIMO</h4>
+            <ul style={{ listStyleType: "none", padding: 0 }}>
+              <li>
+                <a href="#" style={{ color: "white", textDecoration: "none" }}>
+                  Trust & Safety
+                </a>
+              </li>
+              <li>
+                <a href="#" style={{ color: "white", textDecoration: "none" }}>
+                  Travel Credit
+                </a>
+              </li>
+            </ul>
           </div>
           <div>
-            <h5 style={{ margin: 0, color: "black" }}>Hosting</h5>
-            <a href="#" style={{ color: "black", textDecoration: "none" }}>
-              Why Host
-            </a>
-            <a href="#" style={{ color: "black", textDecoration: "none" }}>
-              Hospitality
-            </a>
+            <h4 style={{ margin: 0, color: "black" }}>Поддержка</h4>
+            <ul style={{ listStyleType: "none", padding: 0 }}>
+              <li>
+                <a href="#" style={{ color: "white", textDecoration: "none" }}>
+                  Why Host
+                </a>
+              </li>
+              <li>
+                <a href="#" style={{ color: "white", textDecoration: "none" }}>
+                  Hospitality
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </section>
-      <div style={{ height: "1px", backgroundColor: "gray" }}></div>
-      <p style={{ color: "black", textAlign: "center", marginTop: "10px" }}>
+      <div style={{ height: "1px", backgroundColor: "white" }}></div>
+      <p
+        style={{
+          color: "white",
+          textAlign: "center",
+          marginTop: "10px",
+          fontWeight: "600",
+        }}
+      >
         © 2023 OIMO, Inc. All rights reserved
       </p>
     </footer>
