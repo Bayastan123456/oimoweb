420e6bd57fa8d3a3ad34b3b85ff49f9e211cf97b
Bayastan <abakirovbayastan@gmail.com>
2023-06-14 10:45:23 +0300

    create

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,114 @@
 import React from "react";
+import { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+import { Box, Button, Typography } from "@mui/material";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty("--progress", 1 - progress);
+    progressContent.current.textContent = `${Math.ceil(time / 2000)}s`;
+  };
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        // autoplay={{
+        //   delay: 10000  ,
+        //   disableOnInteraction: false,
+        // }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        onAutoplayTimeLeft={onAutoplayTimeLeft}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Box
+            sx={{
+              backgroundImage:
+                "url(https://img.freepik.com/premium-photo/natural-landscape-lonely-yurt-against-background-of-field-and-mountains_124507-33917.jpg)",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              width: "100vw",
+              height: "100vh",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          >
+            <Typography
+              variant="h1"
+              sx={{ color: "white", boxShadow: "5px 10px 10px black" }}
+            >
+              Добро пожаловать!!!
+            </Typography>
+            <Button
+              variant="outlined"
+              sx={{ marginTop: "50px", color: "white", backgroundColor: "red" }}
+            >
+              Заказать
+            </Button>
+          </Box>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Box
+            sx={{
+              backgroundImage:
+                "url(https://bogatyr.club/uploads/posts/2023-03/1678776207_bogatyr-club-p-yurta-fon-foni-instagram-1.jpg)",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              width: "100vw",
+              height: "100vh",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          ></Box>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Box
+            sx={{
+              backgroundImage:
+                "url(https://abrakadabra.fun/uploads/posts/2022-01/1642475226_1-abrakadabra-fun-p-fon-yurta-1.jpg)",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              width: "100vw",
+              height: "100vh",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          ></Box>
+        </SwiperSlide>
+        <div className="autoplay-progress" slot="container-end">
+          <svg viewBox="0 0 48 48" ref={progressCircle}>
+            <circle cx="24" cy="24" r="20"></circle>
+          </svg>
+          <span ref={progressContent}></span>
+        </div>
+      </Swiper>
+    </>
+  );
 };
 
 export default HomePage;
